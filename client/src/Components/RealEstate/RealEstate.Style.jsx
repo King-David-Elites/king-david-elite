fbfc157bc1ed0832def40cedfabce386eedd0ee2
Marvellous <adeoyemarvellous7@gmail.com>
2022-07-27 11:45:32 +0100

    Added Background Image for Real Estate

--- client/src/Components/RealEstate/RealEstate.Style.jsx
+++ client/src/Components/RealEstate/RealEstate.Style.jsx
@@ -1,1 +1,22 @@
-import style from "styled-components"
+import styled from "styled-components"
+
+export const EstateProperties = styled.div`
+    display:flex;
+    justify-content:space-between;  
+    align-items:center;  
+    gap:1em;
+    padding:2em;    
+    flex-wrap:wrap;
+    width:100%;
+`
+
+export const PropertyType = styled.div`
+    width: 25em;
+    height: 13em;  
+    display:flex;
+    flex-direction:column;    
+    justify-content:center;    
+    align-items:flex-start;
+    background:red;
+   border-radius: 2em 0;
+`
--- client/src/Components/RealEstate/RealEstateListing.jsx
+++ client/src/Components/RealEstate/RealEstateListing.jsx
@@ -1,33 +1,60 @@
+import { Search } from 'heroicons-react'
 import React from 'react'
-import { 
+import {
   Background,
   HeroSection,
   SearchSection,
-  Search,
+  SearchC,
   Input,
-  Text
+  Text,
+  Body
 } from '../../Components/Cars/Cars.Style'
 import Navbar from '../Navbar/Navbar'
 import realEstatePics from './Image/real-estate-pics.jpg'
-
+import { EstateProperties, PropertyType } from './RealEstate.Style'
+import MOCK_DATA from './MOCK_DATA'
 
 const RealEstateListing = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <Background imageUrl={realEstatePics} >
         <HeroSection>
-          <Text fontSize="2rem">Luxury Real Estate</Text>          
+          <Text fontSize="2rem">Luxury Real Estate</Text>
           <SearchSection>
-            <Search>
-              <Input placeholder='search desired locations'/>
-            </Search>                  
+            <SearchC>
+              <Input placeholder='search desired locations' />
+              <Search width="30px" />
+            </SearchC>
           </SearchSection>
           <Text>One search is all it takes</Text>
-        </HeroSection>          
+        </HeroSection>
       </Background>
+
+      <Body>
+        <Text fontSize="1rem" fontWeight="700" color="black">Luxury Properties For Sale</Text>
+        <EstateProperties>
+          {
+            MOCK_DATA.map((items) => {
+              return (
+                <RealEstate key={items.id} {...items} />
+              )
+            })
+          }
+        </EstateProperties>
+      </Body>
     </>
   )
 }
 
 export default RealEstateListing
+
+const RealEstate = ({ propertyName, image }) => {
+  return (
+    <>
+      <PropertyType>
+        <img src={image} alt="Image" />
+      </PropertyType>
+    </>
+  )
+}
