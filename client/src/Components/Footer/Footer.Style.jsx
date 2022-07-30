9c6086aabf5ea5f0d4ad631a788b2e7dd195be76
Ridwan <ridwanfolayimi@gmail.com>
2022-07-30 20:25:31 +0100

    made some changes in the carSection

--- client/src/Components/Footer/Footer.Style.jsx
+++ client/src/Components/Footer/Footer.Style.jsx
@@ -4,9 +4,26 @@ import theme from '../../application/utils/Theme';
 export const FooterSection = styled.div`
     background:${theme.blackBackgroundColor};
     width:100%;
-    padding:2em;
+    padding:3em 3em 10em 3em;
     display:flex;
     justify-content:space-between;
-    align-items:center;
-    color:${theme.footerColor}
+    align-items:flex-start;    
+    font-weight:light;
+    color:${theme.footerColor};
+`
+
+export const Section = styled.div`
+    display:flex;
+    flex-direction:column;
+    justify-content:flex-start;
+    align-items:flex-start;
+    gap:1em;    
+`
+export const Text =styled.div`
+    color:${props => props.color ? props.color : theme.textColor};
+    font-size:${props => props.fontSize ? props.fontSize : "0.8rem"};
+    font-weight:${props => props.fontWeight ? props.fontWeight : "light"};    
+    margin:${props => props.margin ? props.margin : "0em"};    
+    padding:${props => props.padding ? props.padding : "0em"};  
+    cursor:pointer;  
 `
--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-import {FooterSection} from './Footer.Style';
+import {FooterSection,Section,Text} from './Footer.Style';
 const Footer = () =>{
     return(
         <>
-            <FooterSection>Footer</FooterSection>            
+            <FooterSection>
+                <Section>
+                    <Text fontSize="1rem" fontWeight="700">King David Elites</Text>
+                    <Text>About</Text>
+                    <Text>Team</Text>
+                    <Text>Contact Us</Text>
+                    <Text>Terms And Conditions</Text>
+                </Section>
+                <Section>
+                    <Text fontSize="1rem" fontWeight="700">Categories</Text>
+                    <Text>Real Estates</Text>
+                    <Text>Cars</Text>                                
+                </Section>
+                <Section>
+                    <Text fontSize="1rem" fontWeight="700">Business</Text>
+                    <Text>Affiliate Marketing</Text>
+                    <Text>List With Us</Text>                    
+                </Section>
+                <Section>
+                    <Text fontSize="1rem" fontWeight="700">Social Media</Text>
+                    <Text>Facebook</Text>
+                    <Text>Instagram</Text>
+                    <Text>Twitter</Text>
+                    <Text>LinkedIn</Text>
+                </Section>
+            </FooterSection>            
         </>
     )
 }
