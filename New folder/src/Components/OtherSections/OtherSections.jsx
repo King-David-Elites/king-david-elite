adc4b81b92c9467bdbd5a9b38973fd2b50f9dd9f
DevEmmy <eolaosebikan60@gmail.com>
2022-08-05 15:32:29 +0100

    removed unnecessary extra folder

--- New folder/src/Components/OtherSections/OtherSections.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const OtherSections = () => {
-  return (
-    <div>OtherSections</div>
-  )
-}
-
-export default OtherSections
--- New folder/src/Components/RealEstate/RealEstateListing.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const RealEstateListing = () => {
-  return (
-    <div>RealEstateListing</div>
-  )
-}
-
-export default RealEstateListing
