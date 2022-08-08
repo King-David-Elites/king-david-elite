5ac0e4a3069a21bf7da1cc87f3c8306e49a51cfa
unknown <Olamidelawal1498@email.com>
2022-08-08 19:22:02 +0100

    added dashboard files

--- client/src/Components/Dashboard/Wallet.jsx
+++ client/src/Components/Dashboard/Wallet.jsx
@@ -1,10 +1,77 @@
 import React from 'react'
+import styled from 'styled-components'
 import Dashboard from './Dashboard'
 import { GenericContainer } from './Dashboard.Styles'
+import { Leftwallet } from './Dashboard.Styles'
+import { Rightwallet } from './Dashboard.Styles'
+import { Bottom } from './Dashboard.Styles'
+
+
+
 
 const WalletAsElement = ()=>{
     return(
         <GenericContainer>
+
+          <Leftwallet>
+            <h4>Account Balance</h4>
+            <h2>$32,720.69</h2>
+            <div>
+              <button className='transfer'>Transfer</button>
+            <button>Withdraw</button>
+            <button>Deposit</button>
+            </div>
+            <p> Account ID:123345AD</p>
+          </Leftwallet>
+
+          <Rightwallet>
+
+          </Rightwallet>
+
+          <Bottom>
+            <table>
+              <tr>
+                <th>Transaction</th>
+              </tr>
+              <tr>
+                <td>$1,200.00</td>
+                <td>112233HR</td>
+                <td>Daniels Whales</td>
+                <td>16 July,2022</td>
+                <td>2:50pm</td>
+              </tr>
+              <tr>
+                <td>$21,710.00</td>
+                <td>5413691359</td>
+                <td>Lambo Dealer</td>
+                <td>02 July,2022</td>
+                <td>5:00am</td>
+              </tr>
+              <tr>
+                <td>$7,200.00</td>
+                <td>223833AS</td>
+                <td>Leonardo Caprisonne</td>
+                <td>24 March,2022</td>
+                <td>2:51am</td>
+              </tr>
+              <tr>
+                <td>$1,200.00</td>
+                <td>112233HR</td>
+                <td>Daniel Whales</td>
+                <td>16 July,2022</td>
+                <td>7:09pm</td>
+              </tr>
+              <tr>
+                <td>$9,450.00</td>
+                <td>112233FT</td>
+                <td>Daniel Whales</td>
+                <td>19 July,2022</td>
+                <td>2:50am</td>
+              </tr>
+            </table>
+
+          </Bottom>
+         
 
         </GenericContainer>
     )
