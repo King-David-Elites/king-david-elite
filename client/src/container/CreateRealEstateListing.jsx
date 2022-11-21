5c0aa2a6a989eaa0e37255ce3c7c112cb8398674
Folayimi <ridwanfolayimi@gmail.com>
2022-11-22 00:24:08 +0100

    connected to the api

--- client/src/container/CreateRealEstateListing.jsx
+++ client/src/container/CreateRealEstateListing.jsx
@@ -3,14 +3,75 @@ import { useState } from "react";
 import { LocationMarker } from "heroicons-react";
 import { X } from "heroicons-react";
 import { OutProp, InProp, Views } from "./PropertiesContents";
+import axios from "axios";
+import { useEffect } from "react";
 
 const CreateRealEstateListing = () => {
   const [outDoorProp, setOutDoorProp] = useState([]);
   const [inDoorProp, setInDoorProp] = useState([]);
   const [viewProp, setViewProp] = useState([]);
-  const [noOfBedrms, setNoOfBedrms] = useState(0);
-  const [noOfBathrms, setNoOfBathrms] = useState(0);
   const [valid, setValid] = useState(false);
+  const [images, setImages] = useState([]);
+  const [videos, setVideos] = useState([]);
+
+  const [userListings,setUserListings] = useState({
+    title:"",
+    location:"",
+    description:"",
+    images:[],
+    videos:[],
+    price:"",
+    year:"",
+    carCondition:"",
+    engineType:"",
+    colour:"",
+    features:[],
+    model:"",
+    noOfBed:"",
+    noOfBathroom:""
+  })
+
+  useEffect(()=>{
+    
+  },[])
+  const setConfig = (userListings) => {
+    const authToken = localStorage.getItem("token");
+
+    const config = {
+        headers: {
+            Authorization: `Bearer ${authToken}`,
+            ContentType: "application/json",
+        },
+        data:JSON.stringify(userListings),
+        xhrFields: {
+          withCredentials: true
+        },
+        crossDomain: true,
+    };
+
+    return config;
+  };
+
+  const handleChange = (e) =>{
+    let name = e.target.name;
+    let value = e.target.value;
+    setUserListings({...userListings, [name]:value})
+  }
+
+  const saveDetails = () =>{
+    postUserListings(userListings)
+  }
+
+  const postUserListings = async(userListings) =>{
+    await axios.post('listings/upload-list',setConfig(userListings)).then((resp)=>{
+      console.log(resp.message)
+    })
+  }
+
+  const handleSubmit = async ()=>{
+    
+  }
+
   return (
     <>
       <div className="form_Content">
@@ -20,16 +81,29 @@ const CreateRealEstateListing = () => {
         </div>
         <div className="section">
           <p>Title/Name</p>
-          <input type="text" />
+          <input 
+            type="text"
+            name="title"
+            required
+            onChange={handleChange}  
+          />
           <div className="base">Very short headline for your listing</div>
         </div>
         <div className="section">
           <p>Description</p>
-          <textarea type="text" />
+          <textarea 
+            type="text" 
+            name="description"
+            required
+            onChange={handleChange}
+          />
         </div>
         <div className="section">
           <p>Address</p>
-          <input type="text" />
+          <input 
+            type="text" 
+            name="address"
+          />
         </div>
         <div className="section">
           <hr />
@@ -144,32 +218,42 @@ const CreateRealEstateListing = () => {
             <p>No of Bedrooms</p>
             <input
               type="text"
+              name="noOfBed"
               placeholder="0"
-              onChange={(e) => {
-                setNoOfBedrms(e.target.value);
-              }}
+              onChange={handleChange}
             />
           </div>
           <div className="sect">
             <p>No of Bathrooms</p>
             <input
               type="text"
+              name="noOfBathroom"
               placeholder="0"
-              onChange={(e) => {
-                setNoOfBathrms(e.target.value);
-              }}
+              onChange={handleChange}
             />
           </div>
         </div>
         <div className="NumbB">
-          <div className={valid ? "enable" : "disable"}>Save & Continue</div>
+          <div 
+            className={valid ? "enable" : "disable"}
+            onClick={valid && saveDetails}
+          >
+            Save & Continue
+          </div>
         </div>
         <div className="section">
           <hr />
         </div>
         <div className="section">
             <p>Images</p>
-            <input className="dashed" type="file" />
+            <input 
+              className="dashed" 
+              type="file" 
+              onChange={(e)=>{
+                setImages([...images, e.target.value])
+                setUserListings({...userListings, images:images})        
+              }}
+            />
             <div className="base">
                 mark and upload more than one high-quality images, listings with low quality images may be rejected
             </div>
@@ -185,7 +269,11 @@ const CreateRealEstateListing = () => {
             <div className="sect">
                 <p>Price</p>
                 <div className="price">
-                    <input type="text" />
+                    <input 
+                      name="price"
+                      onChange={handleChange}
+                      type="text" 
+                    />
                     <select>
                         <option>USD</option>
                         <option>NGN</option>
