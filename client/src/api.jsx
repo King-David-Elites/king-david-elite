32724ec0d09ed116758636cb01fa4226471fd48c
DevEmmy <eolaosebikan60@gmail.com>
2022-12-22 02:50:20 -0800

    added a global api

--- client/src/api.jsx
+++ client/src/api.jsx
@@ -3,7 +3,7 @@ const debug = process.env.DEBUG;
 const local_url = process.env.LOCAL_URI;
 const production_url = process.env.PRODUCTION_URL
 
-let globalApi = "http://localhost:9099";
+let globalApi = "https://kde.cyclic.app";
 
 // if(debug == "false"){
 //     globalApi = production_url;
