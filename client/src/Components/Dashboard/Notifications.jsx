b1d420520790fa93be924ba4be159764b7913505
DevEmmy <eolaosebikan60@gmail.com>
2022-12-24 07:29:49 -0800

    updated notifications

--- client/src/Components/Dashboard/Notifications.jsx
+++ client/src/Components/Dashboard/Notifications.jsx
@@ -7,26 +7,70 @@ import { FaSearch, FaCheckCircle } from "react-icons/fa"
 import { useState } from 'react'
 import ReadMoreReadLess from './ReadMoreReadLess'
 import theme from '../../application/utils/Theme'
+import { useGetUserDetails } from '../../application/hooks/queryhooks'
+import axios from 'axios'
+import globalApi from '../../api'
+import { setConfig } from '../../infrastructure/api/user/userRequest'
+import { useEffect } from 'react'
 
 const NotificationAsElement = () => {
+    const data = useGetUserDetails;
+    const [notifications, setNotifications] = useState([])
+
+    const getNotifications = ()=>{
+        axios.get(`${globalApi}/notifications/all`, setConfig())
+        .then(resp => setNotifications(resp.data))
+        .catch(err => console.log(err))
+    }
+
+    useEffect(()=>{
+        getNotifications()
+    }, [globalApi])
     return (
         <NotificationContainer>
+            {
+                notifications.map((notification, i)=>{
+                    return(
+                        <ReadMoreReadLess key={i} 
+                            message = {notification.message}
+                            title = {notification.title}
+                            time = {notification.createdAt}
+                        />
+                            
+                    )
+                })
+            }
+            <ReadMoreReadLess
+                title="Deposit Successful"
+                message=" The system has detected that your account is logged in from an unused IP address."
+                time="4:30pm"
+             />
+               
+{/* 
             <ReadMoreReadLess>
                 The system has detected that your account is logged in from an unused IP address.
             </ReadMoreReadLess>
 
             <ReadMoreReadLess>
                 The system has detected that your account is logged in from an unused IP address.
-            </ReadMoreReadLess>
-
-            <ReadMoreReadLess>
-                The system has detected that your account is logged in from an unused IP address.
-            </ReadMoreReadLess>
+            </ReadMoreReadLess> */}
         </NotificationContainer>
     )
 }
 
 const MobileNotification = () => {
+    const [notifications, setNotifications] = useState([])
+
+    const getNotifications = ()=>{
+        axios.get(`${globalApi}/notifications/all`, setConfig())
+        .then(resp => setNotifications(resp.data))
+        .catch(err => console.log(err))
+    }
+
+    useEffect(()=>{
+        getNotifications()
+    }, [globalApi])
+    
     return (
         <MobileDashboardContainer>
             <div className='upper-sect'>
@@ -45,17 +89,25 @@ const MobileNotification = () => {
 
             </div>
 
-            <ReadMoreReadLess>
-                You have successfully made a deposit of $4,000 to your account at 10 - 06 - 2022. If you don’t recogn...
-            </ReadMoreReadLess>
+            {
+                notifications.map((notification, i)=>{
+                    return(
+                        <ReadMoreReadLess key={i} 
+                            message = {notification.message}
+                            title = {notification.title}
+                            time = {notification.createdAt}
+                        />
+                            
+                    )
+                })
+            }
+            <ReadMoreReadLess
+                title="Deposit Successful"
+                message=" The system has detected that your account is logged in from an unused IP address."
+                time="4:30pm"
+             />
 
-            <ReadMoreReadLess>
-                You have successfully made a deposit of $4,000 to your account at 10 - 06 - 2022. If you don’t recogn...
-            </ReadMoreReadLess>
-
-            <ReadMoreReadLess>
-                You have successfully made a deposit of $4,000 to your account at 10 - 06 - 2022. If you don’t recogn...
-            </ReadMoreReadLess>
+            
         </MobileDashboardContainer>
     );
 }
