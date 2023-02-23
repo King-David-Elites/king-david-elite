8f35a24a1a9966caf747824a563dccb78cc98ea8
DevEmmy <eolaosebikan60@gmail.com>
2023-02-23 10:44:15 -0800

    updated stats page

--- client/src/container/ProfileStat.jsx
+++ client/src/container/ProfileStat.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import block from "../Components/Home/images/block.png"
 import { List, Overview, Post } from "./ProfileStat.style"
 import { FaLongArrowAltUp, FaCalendarMinus, FaChevronDown } from 'react-icons/fa'
@@ -6,63 +6,103 @@ import S2 from "../Components/Home/images/S2.jpg"
 import S1 from "../Components/Home/images/S1.jpg"
 import { FaMapMarker } from "react-icons/fa"
 import { FaHeart } from 'react-icons/fa'
+import styled from "styled-components"
+import { Line } from 'react-chartjs-2';
+import { useGetUserDetails } from '../application/hooks/queryhooks'
 
 export default function ProfileStat() {
 
+  const [user, setUser] = useState(useGetUserDetails())
+  
+
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Sales',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgba(75,192,192,1)',
+        tension: 0.1
+      }
+    ]
+  };
+  
+
   return (
-    <List>
-      {/* <Overview>
-        <div className='account'>
-          <p>Account Type : Free</p>
-          <div className='overview-div'>
-            <h3>Overview</h3>
-            <img className='block' src={block} alt="blockPics" />
-          </div>
+    <StatContainer>
+      <div className="accountType">
+        <p>Account Type : *Free</p>
+      </div>
 
-          <div>
-            <FaCalendarMinus className='minus_icon' />
-            <input
-             placeholder='Last 30 Days'
-              />
-            <FaChevronDown className='down_icon' />
+      <div className="data">
+        <div className="overview">
+          <h3>Overview</h3>
+        </div>
+
+        <div className="info">
+          <p className='top'>Page Views</p>
+          <div className="value">
+            <h2>{user.pageViews.value}</h2><p>from {user.pageViews.users.length} users</p>
           </div>
         </div>
 
-        <div className='box'>
-          <div className='page'>
-            <h6>Page Views</h6>
-            <div>
-              <p><span>158</span>from 129</p>
-              <div className='d'>
-                <FaLongArrowAltUp
-                  className='arrow'
-                  color='#3EC73B' />
-                <p>+22.5%</p>
-              </div>
-            </div>
-
+        <div className="info">
+          <p className='top'>Listing Saves</p>
+          <div className="value">
+            <h2>{user.totalSaved.value}</h2><p>from {user.totalSaved.users.length} users</p>
           </div>
+        </div>        
+      </div>
 
-          <div className='page'>
-            <h6>Listing Saves</h6>
-            <div>
-              <p><span>15</span>from 9</p>
-              <div className='d'>
-                <FaLongArrowAltUp
-                  className='arrow'
-                  color='#3EC73B' />
-                <p>+22%</p>
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-      </Overview> */}
-{/* <h3>Listings</h3> */}
-
-
-      <p>Over view</p>
-    </List>
+      {/* <Line data={data} /> */}
+      
+    </StatContainer>
   )
 }
+
+const StatContainer = styled.div`
+width: 80%;
+margin: 20px auto;
+  .accountType{
+    p{
+      color: #737373;
+      font-size: 12px;
+    }
+  }
+
+  .data{
+    display: grid;
+    grid-template-columns: 1.5fr 2fr 2fr;
+    gap: 10px;
+  }
+
+  .info{
+    background: rgba(242, 242, 242, 0.5);
+    border-radius: 8px 0px 0px 8px;
+    padding: 20px;
+
+    p.top{
+      font-size: 14px;
+      font-weight: 700;
+    }
+
+    .value{
+      display: flex;
+      align-items: baseline;
+      justify-content: baseline;
+      gap: 5px;
+
+      h2{
+        color: blue;
+        font-size: 30px;
+        padding: 0;
+        font-weight: 800;
+      }
+
+      p{
+        font-size: 15px
+      }
+    }
+  }
+`
