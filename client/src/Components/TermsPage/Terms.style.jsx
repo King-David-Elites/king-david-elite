ab2d3ec164431938794e3db1944353737d2aae5e
LawalOlamide <90286428+LawalOlamide@users.noreply.github.com>
2023-02-26 08:57:13 +0100

    lawal changes

---
+++ client/src/Components/TermsPage/Terms.style.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+.nav{
+    background-color: rgba(0,0,0,.8);
+    padding:1em;
+    box-shadow:2px 1px 2px rgba(0,0,0,.1);
+    display: flex;
+
+    .mini{
+        padding-left:1em;
+        display: flex;
+        align-items:center;
+        gap:.5em;
+        color:#F2BE5C;
+    }
+}
+
+.cont{
+    margin:1em auto;
+
+    h2{
+        text-align: center;
+        font-size:2rem;
+    }
+
+    p{
+        font-size:.9rem;
+        padding: .5em 6em;
+    }
+
+    b{
+        display:block;
+    }
+
+    i{
+        display: block;
+    }
+}
+
+@media only screen and (max-width:500px){   
+
+    .cont{
+    
+        h2{
+            font-size:1.6rem;
+        }
+
+        p{
+            font-size:1rem;
+            padding: .5em 3em;
+        }
+
+    }
+
+
+}
+
+`
