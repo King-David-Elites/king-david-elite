d4fabb69a7b1994e5c1fb3d0fa074931e6dc903b
Marvellous <adeoyemarvellous7@gmail.com>
2023-02-28 22:19:40 +0100

    ...

--- client/src/Components/Categories/RealEstate/RealEstateListing.jsx
+++ client/src/Components/Categories/RealEstate/RealEstateListing.jsx
@@ -35,6 +35,7 @@ import Loader from "../../Loader/Loader";
 import theme from "../../../application/utils/Theme";
 import { ImCross } from "react-icons/im";
 import DropdownInput from "../../inputs/DropdownInput";
+import { BiArrowBack } from "react-icons/bi";
 
 const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop);
 
@@ -49,40 +50,26 @@ const RealEstateListing = () => {
   const [loader, setLoader] = useState(false);
   const [estateId, setEstateId] = useState(1);
   const [animation, setAnimation] = useState(graduallyAppear);
-  const [filter, setFilter] = useState(false);
   const [title, setTitle] = useState('');
   const [list, setList] = useState([]);
   const [query, setQuery] = useState("title");
   const [activePage, setActivePage] = useState(false);
   const [minValue, set_minValue] = useState(25);
   const [maxValue, set_maxValue] = useState(75);
+  const [view, setView] = useState('Porch');
+  const [stage, setStage] = useState(0);
+
   const handleInput = (e) => {
     set_minValue(e.minValue);
     set_maxValue(e.maxValue);
+    setQuery("price");
   };
 
   const showPage = () => {
+    setStage(1);
     setActivePage(!activePage);
   }
 
-  const options = [
-    {
-      Id: 0,
-      title: "title"
-    },
-    {
-      id: 1,
-      title: "noOfBed"
-    },
-    {
-      id: 2,
-      title: "price"
-    },
-    {
-      id: 3,
-      title: "location"
-    }
-  ]
 
   const views = [
     {
@@ -105,40 +92,6 @@ const RealEstateListing = () => {
       id: 4,
       title: "Terrace"
     },
-    {
-      id: 5,
-      title: "Balcony"
-    }
-  ]
-
-  const noOfBedrooms = [
-    {
-      Id: 1,
-    },
-    {
-      id: 2,
-    },
-    {
-      id: 3,
-    },
-    {
-      id: 4,
-    },
-    {
-      id: 5,
-    },
-    {
-      id: 6,
-    },
-    {
-      id: 7,
-    },
-    {
-      id: 8,
-    },
-    {
-      id: 9,
-    },
   ]
 
   const searchListing = () => {
@@ -149,6 +102,7 @@ const RealEstateListing = () => {
       .then(resp => {
         setList(resp.data)
         setLoader(false)
+        setStage(0);
       })
       .catch(err => console.log(err))
   }
@@ -178,160 +132,149 @@ const RealEstateListing = () => {
   }, [estateId]);
   return (
     <>
-      <Navbar active={1} />
-      <div className={activePage ? 'p-2 fixed w-[100vw] top-0 bottom-0 h-[100%] bg-white transition-[1s] z-10' : 'hidden'}>
-        <div className='closed p-4'>
-          <div className="relative w-full lg:max-w-sm flex flex-col gap-6">
-            {/* <select className="w-full p-3 text-gray-500 bg-white border rounded-md shadow-sm outline-none focus:border-theme-color">
+      {
+        stage == 1 && <div className='p-2 fixed w-[100vw] top-0 bottom-0 h-[100%] bg-white transition-[1s] z-20'>
+          <div className='p-4'>
+            <div className="relative w-full lg:max-w-sm flex flex-col gap-6">
+              <div className="h-[40px] flex items-center gap-4">
+                <BiArrowBack size={30} className="opacity-[0.5]" onClick={() => setStage(0)} />
+                <p className="font-bold text-[20px]">Filters</p>
+              </div>
+              {/* <select className="w-full p-3 text-gray-500 bg-white border rounded-md shadow-sm outline-none focus:border-theme-color">
               <option>Location</option>
               <option>Laravel 9 with React</option>
               <option>React with Tailwind CSS</option>
               <option>React With Headless UI</option>
             </select> */}
-            <h3 className="font-bold">Location</h3>
-            <input type="text" className="w-full rounded-md p-2 text-gray-500 bg-white border outline-none focus:border-theme-color" />
+              <h3 className="font-bold">Location</h3>
+              <input type="text" className="w-full rounded-md p-2 text-gray-500 bg-white border outline-none focus:border-theme-color" onClick={() => setQuery("location")} onChange={(e) => setTitle(e.target.value)} />
 
-            <h3 className="font-bold">Views</h3>
-            <div className="flex gap-3 flex-wrap">
+              <h3 className="font-bold">Title</h3>
+              <input type="text" className="w-full rounded-md p-2 text-gray-500 bg-white border outline-none focus:border-theme-color" onClick={() => setQuery("title")} onChange={(e) => setTitle(e.target.value)} />
+
+              <h3 className="font-bold">Views</h3>
+              <div className="flex gap-3 flex-wrap">
+                {
+                  views.map((v, i) => {
+                    return (
+                      <span className={`bg-gray-300 font-medium text-[15px] whitespace-nowrap h-full flex justify-center rounded-3xl p-3 ${view == v.title ? `rounded-md h-[10%]  bg-theme-color hover:opacity-[100%] items-center` : ""}`} key={i} onChange={(e) => setTitle(e.target.value)} onClick={() => { setQuery("view"); setView(v.title) }}>{v.title}</span>
+                    )
+                  })
+                }
+              </div>
+
+              <h3 className="font-bold">Bedrooms</h3>
+              <div className="rounded-md"><input className="w-full p-2 text-gray-500 bg-white border outline-none focus:border-theme-color" type="number" min={0} onChange={(e) => setTitle(e.target.value)} onClick={() => setQuery("noOfBed")} /></div>
+
+              <h3 className="font-bold">Bathrooms</h3>
+              <div className="rounded-md"><input className="w-full p-2 text-gray-500 bg-white border outline-none focus:border-theme-color" type="number" min={0} onChange={(e) => setTitle(e.target.value)} onClick={() => setQuery("noOfBathroom")} /></div>
+
+
+              <h3 className="font-bold">Price</h3>
+              <MultiRangeSlider
+                min={0}
+                max={10000}
+                step={5}
+                minValue={minValue}
+                maxValue={maxValue}
+                onInput={(e) => {
+                  handleInput(e);
+                }}
+                onChange={() => setTitle(maxValue - minValue)}
+                label={false}
+                ruler={false}
+                style={{ border: "none", boxShadow: "none", padding: "15px 10px" }}
+                barLeftColor={theme.backgroundColor}
+                barInnerColor={theme.color}
+                barRightColor={theme.backgroundColor}
+                thumbLeftColor={theme.color}
+                thumbRightColor={theme.color}
+              />
+
+              <div className="flex gap-5 w-full mb-5 items-center">
+                <input type="number" className="border-2 p-2 w-[50%]" placeholder={minValue} onChange={(e) => setTitle(e.target.value)} onClick={() => setQuery("noOfBathroom")} />
+                <span className="font-bold text-[15px]">-</span>
+                <input type="number" className="border-2 p-2 w-[50%]" onChange={(e) => setTitle(e.target.value)} onClick={() => setQuery("noOfBathroom")} placeholder={maxValue} />
+              </div>
+
+              <MainButton width='100%' padding='24px 12px' onClick={() => { searchListing(); }}>Apply Filter {
+                loader && <SpinnerCircular color="white" className="mr-4" secondaryColor={theme.color} size={50} thickness={150} />
+              }</MainButton>
+            </div>
+
+          </div>
+        </div >
+      }
+      {
+        stage == 0 && <>
+          <Navbar active={1} />
+          <Background ref={top}>
+            {EstateAnimation.map((item) => {
+              if (item.id === estateId) {
+                return (
+                  <motion.div
+                    className="bgImage"
+                    variants={animation}
+                    initial="hidden"
+                    animate="visible"
+                  >
+                    <img src={item.img} alt="real estates" />
+                  </motion.div>
+                );
+              }
+            })}
+            <HeroSection>
+              <Text fontSize="2rem">Luxury Real Estate</Text>
+              <SearchSection>
+                <SearchC>
+                  <Input placeholder="search desired locations" onChange={(e) => setTitle(e.target.value)} />
+                  <Search width="30px" onClick={(e) => searchListing(title)} className="cursor-pointer" />
+                </SearchC>
+                <FilterBox onClick={() => showPage()}>
+                  <MainButton width="60px" >Filter</MainButton>
+                  <Adjustments width="30px" />
+                </FilterBox>
+              </SearchSection>
+              <Text>One search is all it takes</Text>
+            </HeroSection>
+          </Background>
+
+          <Body>
+            <Text fontSize="1rem" fontWeight="700" color="black">
+              Luxury Properties For Sale
+            </Text>
+            {
+              loader && <SpinnerCircular color="white" className="flex justify-center" secondaryColor={theme.color} size={50} thickness={150} />
+            }
+            <GridContainer>
               {
-                views.map((v, i) => {
+                (!loader && title && list.length > 0) && list.map((items) => {
                   return (
-                    <span className="bg-gray-300 font-medium text-[15px] whitespace-nowrap h-full flex justify-center rounded-3xl p-3" key={i}>{v.title}</span>
-                  )
+                    <Listing key={items._id} list={items} />
+                  );
                 })
               }
-            </div>
-
-            <h3 className="font-bold">Bedrooms</h3>
-            <div className="rounded-md"><input className="w-full p-2 text-gray-500 bg-white border outline-none focus:border-theme-color" type="number" min={0} /></div>
-
-            <h3 className="font-bold">Bathrooms</h3>
-            <div className="rounded-md"><input className="w-full p-2 text-gray-500 bg-white border outline-none focus:border-theme-color" type="number" min={0} /></div>
-
-
-            <h3 className="font-bold">Price</h3>
-            <MultiRangeSlider
-              min={0}
-              max={10000}
-              step={5}
-              minValue={minValue}
-              maxValue={maxValue}
-              onInput={(e) => {
-                handleInput(e);
-              }}
-              label={false}
-              ruler={false}
-              style={{ border: "none", boxShadow: "none", padding: "15px 10px" }}
-              barLeftColor={theme.color}
-              barInnerColor={theme.color}
-              barRightColor={theme.color}
-              thumbLeftColor="lime"
-              thumbRightColor="lime"
-            />
-
-            <MainButton width='100%' padding='24px 12px'>Apply Filter</MainButton>
-          </div>
-
-
-          {/* <ImCross className='close' color='#000' onClick={showPage} /> */}
-        </div>
-      </div >
-      <Background ref={top}>
-        {EstateAnimation.map((item) => {
-          if (item.id === estateId) {
-            return (
-              <motion.div
-                className="bgImage"
-                variants={animation}
-                initial="hidden"
-                animate="visible"
-              >
-                <img src={item.img} alt="real estates" />
-              </motion.div>
-            );
-          }
-        })}
-        <HeroSection>
-          <Text fontSize="2rem">Luxury Real Estate</Text>
-          {/* <SearchSection>
-            <SearchC>
-              <Input placeholder="search desired locations" onChange={(e) => setTitle(e.target.value)} />
-              <Search width="30px" onClick={(e) => searchListing(title)} className="cursor-pointer" />
-            </SearchC>
-            <div className="flex flex-col justify-center items-center">
-              <FilterBox onClick={() => setFilter(!filter)}>
-                <MainButton width="60px">Filter</MainButton>
-                <Adjustments width="30px" />
-              </FilterBox>
               {
-                filter == true &&
-                <ul className='mt-[2px] shadow-md shadow-gray-300 rounded-md p-1 md:p-2 overflow-hidden bg-white cursor-pointer max-h-[180px] overflow-y-auto w-[100%]'>
-                  {
-                    options.map((o) => {
-                      return (
-                        <>
-                          <p onClick={() => { setQuery(o.title); setFilter(!filter) }}>{o.title.toUpperCase()}</p>
-                          <hr />
-                        </>
-                      )
-                    })
-                  }
-                </ul>
+                !loader && title && list.length == 0 && <p className="font-bold text-base md:text-xl">No list found</p>
               }
-            </div>
+              {
+                !loader && !title && listing.length > 0 && listing.map((items) => {
+                  return (
+                    <Listing key={items._id} list={items} />
+                  );
+                })
+              }
+            </GridContainer>
+          </Body>
+          <Banner category="Real Estate" />
+          <Text color="black" fontSize="16px" margin="5%">
+            Indulge in opulence with King David Elites. Our online marketplace boast a collection of exquiste, high-end properties that exude luxury living.From stunning homes to sprawling estates and opulent apartment,our offering cater to all your residential,investment and for-profit needs. Our secure payment system ensures hassle-free transactions, with the option to transfer funds directly to verifies vendors or through our 1% transaction fee escrow account which further secures your funds and grants you access to our team of experts who provide professional advisory services and arranges luxurious property inspection, setting a new standardof class and sophistication.
 
-          </SearchSection> */}
-          <SearchSection>
-            <SearchC>
-              <Input placeholder="search desired location" />
-              <Search width="30px" />
-            </SearchC>
-            <FilterBox>
-              <MainButton width="60px" onClick={showPage}>Filter</MainButton>
-              <Adjustments width="30px" />
-            </FilterBox>
-          </SearchSection>
-          <Text>One search is all it takes</Text>
-        </HeroSection>
-      </Background>
+          </Text>
+          <Footer />
+        </>
+      }
 
-
-      <Body>
-        <Text fontSize="1rem" fontWeight="700" color="black">
-          Luxury Properties For Sale
-        </Text>
-
-        {
-          loader && <SpinnerCircular color="white" className="flex justify-center" secondaryColor={theme.color} size={50} thickness={150} />
-        }
-        <GridContainer>
-          {
-            (!loader && title && list.length > 0) && list.map((items) => {
-              return (
-                // <RealEstate key={items._id} {...items} />
-                <Listing key={items._id} list={items} />
-              );
-            })
-          }
-          {
-            !loader && title && list.length == 0 && <p className="font-bold text-base md:text-xl">No list found</p>
-          }
-          {
-            !loader && !title && listing.length > 0 && listing.map((items) => {
-              return (
-                // <RealEstate key={items._id} {...items} />
-                <Listing key={items._id} list={items} />
-              );
-            })
-          }
-        </GridContainer>
-      </Body>
-      <Banner category="Real Estate" />
-      <Text color="black" fontSize="16px" margin="5%">
-        Indulge in opulence with King David Elites. Our online marketplace boast a collection of exquiste, high-end properties that exude luxury living.From stunning homes to sprawling estates and opulent apartment,our offering cater to all your residential,investment and for-profit needs. Our secure payment system ensures hassle-free transactions, with the option to transfer funds directly to verifies vendors or through our 1% transaction fee escrow account which further secures your funds and grants you access to our team of experts who provide professional advisory services and arranges luxurious property inspection, setting a new standardof class and sophistication.
-
-      </Text>
-      <Footer />
     </>
   );
 };
