9441593498d7921fc92ffa8bc6d2e2fe543a0f08
Marvellous <adeoyemarvellous7@gmail.com>
2023-03-06 16:43:15 +0100

    added love icon

--- client/src/Components/Listing/Listing.jsx
+++ client/src/Components/Listing/Listing.jsx
@@ -1,23 +1,26 @@
-import { LocationMarker } from 'heroicons-react'
+import { Heart, HeartOutline, LocationMarker } from 'heroicons-react'
 import React from 'react'
+import { useState } from 'react'
 import { FaCheckCircle } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 import { Container } from './Listing.styled'
 
-const Listing = ({list, type}) => {
-  const truncate = (text, txtLimit)=>{
+const Listing = ({ list, type }) => {
+  const [loved, setLoved] = useState(false);
+
+  const truncate = (text, txtLimit) => {
     let limit = txtLimit || 30
-    if(text.length > limit){
+    if (text.length > limit) {
       return text.substring(0, limit) + "..."
     }
-    else{
+    else {
       return text
     }
   }
 
   const navigate = useNavigate()
 
-  const redirect = (id)=>{
+  const redirect = (id) => {
     navigate(id)
   }
   return (
@@ -28,8 +31,20 @@ const Listing = ({list, type}) => {
         <FaCheckCircle />
       </div>
       <div className="image">
-        <img src={list.images[0]} alt="" />
-        <div className="view" onClick={()=>{redirect(list._id)}}>
+        <img src={list.images[0]} alt="" className='relative' />
+        <div
+          onClick={() => {
+            setLoved(!loved);
+          }}
+          className='absolute top-3 left-3 bg-[#ffffff33] rounded-full p-2'
+        >
+          {loved ? (
+            <Heart color="#FFDF00" />
+          ) : (
+            <HeartOutline color="yellow" />
+          )}
+        </div>
+        <div className="view" onClick={() => { redirect(list._id) }}>
           Read More
         </div>
       </div>
@@ -42,7 +57,7 @@ const Listing = ({list, type}) => {
       <p className="price">
         ${list.price}
       </p>
-      
+
       <p className="description">
         {/* {
           truncate(list.description, 50)
