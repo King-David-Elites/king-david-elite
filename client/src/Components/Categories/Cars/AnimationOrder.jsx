88316877de539d7e7d892ceca74fd14c2fc5c165
Folayimi <ridwanfolayimi@gmail.com>
2023-04-04 21:52:28 +0100

    added and changed some images

--- client/src/Components/Categories/Cars/AnimationOrder.jsx
+++ client/src/Components/Categories/Cars/AnimationOrder.jsx
@@ -5,6 +5,9 @@ import vehicle4 from "./Img/vehicle4.jpg";
 import vehicle5 from "./Img/vehicle5.jpg";
 import vehicle6 from "./Img/vehicle6.jpg";
 import vehicle7 from "./Img/vehicle7.jpg";
+import vehicle8 from "./Img/vehicle8.jfif";
+import vehicle9 from "./Img/vehicle9.jpg";
+import vehicle10 from "./Img/vehicle10.jfif";
 
 import estate1 from "./Img/estate1.jpg";
 import estate2 from "./Img/estate2.jpg";
@@ -13,11 +16,7 @@ import estate4 from "./Img/estate4.jpg";
 import estate5 from "./Img/estate5.jpg";
 
 import home1 from "./Img/home1.jpg";
-import home2 from "./Img/home2.jpg";
-import home3 from "./Img/home3.jpg";
-import home4 from "./Img/home4.jpg";
-import home5 from "./Img/home5.jpg";
-import home from "./Img/home.jpg";
+import h1 from "../../Home/images/h1.jpg"
 
 import about1 from "./Img/about1.jpg";
 import about2 from "./Img/about2.jpg";
@@ -31,6 +30,18 @@ import about9 from "./Img/about9.jpg";
 
 import c4 from "../LuxuryService/images/pics4.png"
 import c5 from "../LuxuryService/images/pics5.jpg"
+
+import cl1 from "../Collectibles/Image/cl1.jpg";
+import cl2 from "../Collectibles/Image/cl2.jpg";
+import cl3 from "../Collectibles/Image/cl3.jpg";
+import cl4 from "../Collectibles/Image/cl4.jpg";
+import cl5 from "../Collectibles/Image/cl5.jpg";
+import cl6 from "../Collectibles/Image/cl6.jpg";
+import cl7 from "../Collectibles/Image/cl7.jpg";
+import cl8 from "../Collectibles/Image/cl8.jpg";
+import cl9 from "../Collectibles/Image/cl9.jpg";
+import cl10 from "../Collectibles/Image/cl10.jpg";
+import cl11 from "../Collectibles/Image/cl1.jpg";
 
 export const graduallyAppear = {
   hidden: {
@@ -64,6 +75,9 @@ export const CarAnimation = [
   { id: 5, img: vehicle4 },
   { id: 6, img: vehicle6 },
   { id: 7, img: vehicle5 },
+  { id: 8, img: vehicle8 },
+  { id: 9, img: vehicle9 },
+  { id: 10, img: vehicle10 },
 ];
 
 export const EstateAnimation = [
@@ -81,8 +95,9 @@ export const HomeAnimation = [
   { id: 5, img: about3 }, 
   { id: 6, img: estate2},
   { id: 7, img: vehicle7},
-  { id: 8, img: about6 },
-  { id: 9, img: about7 }
+  { id: 8, img: h1},
+  { id: 9, img: about6 },
+  { id: 10, img: about7 }
 ];
 
 export const AboutAnimation = [
@@ -98,13 +113,15 @@ export const AboutAnimation = [
 ];
 
 export const CollectibleAnimation = [
-  { id: 1, img: about1 },
-  { id: 2, img: about2 },
-  { id: 3, img: about3 },
-  { id: 4, img: about4 },
-  { id: 5, img: about5 },
-  { id: 6, img: about6 },
-  { id: 7, img: about7 },
-  { id: 8, img: about8 },
-  { id: 9, img: about9 },
+  { id: 1, img: cl1 },
+  { id: 2, img: cl2 },
+  { id: 3, img: cl3 },
+  { id: 4, img: cl4 },
+  { id: 5, img: cl5 },
+  { id: 6, img: cl6 },
+  { id: 7, img: cl7 },
+  { id: 8, img: cl8 },
+  { id: 9, img: cl9 },
+  { id: 10, img: cl10 },
+  { id: 11, img: cl11 },
 ];
